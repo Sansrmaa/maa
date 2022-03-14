6d4af4548fd06b57fb12c572f4353a718aa2e1a7
sansarmaa <sannie_2002@yahoo.com>
2022-03-14 20:04:33 +0800

    database connecttedddd

--- app.js
+++ app.js
@@ -1,2 +1,35 @@
 const express = require('express');
-const mysql = require('mysql');
+const dotenv = require('dotenv');
+const connection = require('./database');
+dotenv.config();
+const app = express();
+
+var id = 5;
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+    connection.query('SELECT * from fck', (err, rows, fields) => {
+        if (err) throw err;
+        console.log(rows);
+      })
+
+  })
+
+  app.get('/lalar', (req, res) => {
+    res.send('hi lalaraa')
+    id++;
+    connection.query(`INSERT INTO fck (idfck, fname, lname, age) VALUES (${id}, "hi", "lalar", 18)`, (err, rows, fields) => {
+        if (err) throw err;
+        console.log("amjilttai hadgallaa");
+      })
+  })
+
+app.listen(process.env.PORT,() => {
+    console.log(`fucking app is listening on PORT --> ${process.env.PORT}`);
+    connection.connect((err) => {
+        if(err) throw err;
+        console.log("Database connected");
+    });
+    
+  });
+
+  
